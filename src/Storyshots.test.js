94e9e3122b11f955ef89ea5fe38e52595232506d
phil <phil.rice@validoc.org>
2021-10-24 09:28:13 +0200

    disabling storyshots in cicd for now

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,9 +1,13 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
-if (process.env.CI) {
+if (process.env.GITHUB_WORKFLOW) {
     describe("behavior of snapshots in CI/CD", () => {
         it("still needs to be configured", () => {
-          console.log("Snapshots are disabled in the CI/CD pipeline.")
+            console.log("Snapshots are disabled in the CI/CD pipeline.")
+            console.log("GITHUB_WORKFLOW", process.env.GITHUB_WORKFLOW)
+            console.log("USERNAME", process.env.USERNAME)
+            console.log("HOMEPATH", process.env.HOMEPATH)
+            console.log("CI", process.env.CI)
         })
     })
 } else {
