89faacb8a49815864bb5599f10e50088421772cc
phil <phil.rice@validoc.org>
2021-11-26 08:31:09 +0100

    Starting account personalisation stories

---
+++ src/examples/accountPersonalisation/accountPersonaliation.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {AccountPersonalisationPage, AccountPersonalisationProps} from "./accountPersonalisation";
+import {sampleAccountPersonalisation} from "./sampleAccountPersonalisation";
+
+
+
+export default {
+    component: AccountPersonalisationPage,
+    title: 'Examples/AccountPersonalisation',
+}
+
+
+const Template = (args: AccountPersonalisationProps) =>
+    <AccountPersonalisationPage {...args} />;
+
+export const LoadingFalse = Template.bind({});
+// @ts-ignore
+LoadingFalse.args = {
+    loading: false,
+    accountPersonalisationDetails: sampleAccountPersonalisation
+}
+export const LoadingTrue = Template.bind({});
+// @ts-ignore
+LoadingTrue.args = {
+    loading: true,
+    accountPersonalisationDetails: sampleAccountPersonalisation
+};
+
+// export const Primary = () => <ButtonTitleValue id='1' titlelabel='title1' titles={sampleTitles} valuelabel='value1' values={sampleValues}/>;
+
+// export const Primary = () => <Values title='Some Title' values={sampleValues} labels={["value1", "value2"]}/>;
---
+++ src/examples/accountPersonalisation/sampleAccountPersonalisation.ts
@@ -0,0 +1,25 @@
+import {AccountPersonalisationDomain, OnePersonalisationDomain} from "./accountPersonalisationDomain";
+
+export const sampleAccountPersonalisation: AccountPersonalisationDomain= {
+    status: 'Received Complete',
+    estimatedCompletionDate: '2020-10-01',
+    sortCode: '10-11-12',
+    bankAccountNumber: '12334353',
+    lastChanged: '2020-09-01',
+    primary:{
+        title: 'Mr',
+        forename: 'Fred',
+        middleName: '',
+        surname: 'Bloggs',
+        personalisation: 'Mr Fred Bloggs'
+    } ,
+    joint: {
+        title: 'Mrs',
+        forename: 'Matilda',
+        middleName: '',
+        surname: 'Bloggs',
+        personalisation: 'Mrs Matilda Bloggs'
+    },
+    primarySelected: false
+
+}
